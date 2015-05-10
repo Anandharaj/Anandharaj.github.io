418f15d846ff565ece2503a3f20d3f1099de33e7
Anandharajan <anandharajan777@gmail.com>
2015-05-10 21:17:15 +0530

    Gulp configuration implemented

--- js/app.js
+++
@@ -1,8 +0,0 @@
-/****************************** KOT Main Module **********************************************/ 
-
-var kot = angular.module('kot', ['LocalStorageModule', 'ui.router']);
-
-/************************** Constants *************************************************/ 
-
-var USERNAME = 'admin';
-var PASSWORD = 'admin@123';
